fe9cef729bddea6f072cb8cdb8ad416e33d46bb2
ludmilkk <ludakapust@gmail.com>
2026-02-09 15:52:56 +0100

    added deploy key param

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -26,7 +26,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       key: DEPLOY_KEY,
-      'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
+      'pre-deploy-local': `scp -i ${DEPLOY_KEY} .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
       'post-deploy': 'cd backend && npm install && npm run build && pm2 startOrReload ecosystem.config.js',
     },
   },
